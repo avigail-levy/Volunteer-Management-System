2fd9c2b2a2b77c616d0e78b01f03fa4641be34e1
zipi-salomon <zipi.3382@gmail.com>
2024-11-17 22:07:53 +0200

    first entity- volunteer

---
+++ DalFacade/DO/Volunteer .cs
@@ -0,0 +1,34 @@
+﻿namespace DO;
+/// <summary>
+/// Volunteer Entity represents a volunteer with all its props
+/// </summary>
+/// <param name="Id">Personal unique ID of the volunteer (as in national id card)</param>
+/// <param name="Name">Volunteer's full name (first name and last name)</param>
+/// <param name="Phone">Volunteer's phone number</param>
+/// <param name="Email">Volunteer's unique email</param>
+/// <param name="Password"></param>
+/// <param name="Address">Full and real address in correct format, of the volunteer. where it is 
+/// currently located and available to receive calls that are at the distance defined for it</param>
+/// <param name="Latitude">A number indicating how far a point on Earth is south or north of the equator</param>
+/// <param name="Longitude">A number indicating how far a point on Earth is east or west of the equator</param>
+/// <param name="Role">"Manager" or "Volunteer"</param>
+/// <param name="Active">Is the volunteer active or inactive (retired from the organization). A retired 
+/// volunteer also has his reading history saved for him, but he cannot handle new readings.</param>
+/// <param name="MaxDistanceForReading">Maximum distance for reading</param>
+/// <param name="DistanceType">Aerial distance, walking distance, driving distance
+///The default is air distance.</param>
+public record Volunteer
+(
+    int Id,
+    string Name,
+    string Phone,
+    string Email,
+    string Password,
+    string Address,
+    double Latitude,
+    double Longitude,
+    Enum Role,
+    bool Active,
+    double MaxDistanceForReading,
+    Enum DistanceType
+);
