c58de9c861f5cef0eed860cacfc21084b85a8e1c
avigail-levy <avigail6282@gmail.com>
2024-11-19 00:03:00 +0200

    points2

--- DalFacade/DO/Call.cs
+++ DalFacade/DO/Call.cs
@@ -3,13 +3,13 @@
 ///  Call Entity represents a call with all its props
 /// </summary>
 /// <param name="Id">Qnique ID number of the call</param>
-/// <param name="CallType">The type of call according to the type of specific system</param>
+/// <param name="CallType">The type of call according to the type of specific system.</param>
 /// <param name="CallDescription">Description of the Call. Detailed details on the Call.</param>
-/// <param name="CallAddress">Full and real address in correct format, of the Call location</param>
+/// <param name="CallAddress">Full and real address in correct format, of the Call location.</param>
 /// <param name="Latitude">A number that indicates how far a point on Earth is south or north of the equator.</param>
 /// <param name="Longitude">A number indicating how far a point on Earth is east or west of the equator.</param>
 /// <param name="OpeningTime">Time (date and time) when the call was opened by the manager.</param>
-/// <param name="MaxTimeFinishCall">Time (date and time) by which the reading should be closed</param>
+/// <param name="MaxTimeFinishCall">Time (date and time) by which the reading should be closed.</param>
 public record Call
 (
     int Id,
--- DalFacade/DO/Volunteer .cs
+++ DalFacade/DO/Volunteer .cs
@@ -2,15 +2,15 @@
 /// <summary>
 /// Volunteer Entity represents a volunteer with all its props
 /// </summary>
-/// <param name="Id">Personal unique ID of the volunteer (as in national id card)</param>
-/// <param name="Name">Volunteer's full name (first name and last name)</param>
-/// <param name="Phone">Volunteer's phone number</param>
-/// <param name="Email">Volunteer's unique email</param>
+/// <param name="Id">Personal unique ID of the volunteer (as in national id card).</param>
+/// <param name="Name">Volunteer's full name (first name and last name).</param>
+/// <param name="Phone">Volunteer's phone number.</param>
+/// <param name="Email">Volunteer's unique email.</param>
 /// <param name="Password"></param>
 /// <param name="Address">Full and real address in correct format, of the volunteer. where it is 
-/// currently located and available to receive calls that are at the distance defined for it</param>
-/// <param name="Latitude">A number indicating how far a point on Earth is south or north of the equator</param>
-/// <param name="Longitude">A number indicating how far a point on Earth is east or west of the equator</param>
+/// currently located and available to receive calls that are at the distance defined for it.</param>
+/// <param name="Latitude">A number indicating how far a point on Earth is south or north of the equator.</param>
+/// <param name="Longitude">A number indicating how far a point on Earth is east or west of the equator.</param>
 /// <param name="Role">"Manager" or "Volunteer"</param>
 /// <param name="Active">Is the volunteer active or inactive (retired from the organization). A retired 
 /// volunteer also has his call history saved for him, but he cannot handle new calls.</param>
