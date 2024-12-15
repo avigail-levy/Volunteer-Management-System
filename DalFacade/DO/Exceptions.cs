b5ff725ec6756a6cec85ecb31ec58445ce022867
avigail-levy <avigail6282@gmail.com>
2024-12-16 00:00:25 +0200

    begining stage 3 (1-3)

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -9,7 +9,7 @@ public class DalDoesNotExistException : Exception
     public DalDoesNotExistException(string? message) : base(message) { }
 }
 /// <summary>
-/// "The object already exists" - the exception will be thrown , for example,
+/// The object already exist - the exception will be thrown , for example,
 /// when trying to add to the list of objects of any type, an object with an
 /// ID number that already exists
 /// </summary>
@@ -18,9 +18,20 @@ public class DalAlreadyExistsException : Exception
 {
     public DalAlreadyExistsException(string? message) : base(message) { }
 }
+/// <summary>
+/// The exception will be thrown when no value is received from the user.
+/// </summary>
 [Serializable]
 public class DalMustValueExeption : Exception
 {
     public DalMustValueExeption(string? message) : base(message) { }
+}
+/// <summary>
+/// Throw an exception when an xml file fails to create.
+/// </summary>
+[Serializable]
+public class DalXMLFileLoadCreateException : Exception
+{
+    public DalXMLFileLoadCreateException(string? message) : base(message) { }
 }
 
