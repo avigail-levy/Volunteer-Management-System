5a64a3a9c660fd91afafb94f715a6d5fe682aa43
avigail-levy <avigail6282@gmail.com>
2025-05-15 13:51:38 +0300

    xaml volunteer add update

--- PL/Volunteer/VolunteerWindow.xaml.cs
+++ PL/Volunteer/VolunteerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,13 +20,45 @@ namespace PL.Volunteer
     /// </summary>
     public partial class VolunteerWindow : Window
     {
-        public BO.Role Role { get; set; } = BO.Role.Volunteer;
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
 
-        public VolunteerWindow()
+        //public BO.Role Role { get; set; } = BO.Role.Volunteer;
+
+        public VolunteerWindow(int id=0)
         {
-
+            ButtonText = id == 0 ? "Add" : "Update";
             InitializeComponent();
+
+            try
+            {
+                CurrentVolunteer = id != 0
+                    ? s_bl.Volunteer.GetVolunteerDetails(id)
+                    : 
+                    new BO.Volunteer() {Id=0,Role=BO.Role.Volunteer,Active=false};
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
         }
+        public BO.Volunteer? CurrentVolunteer
+        {
+            get { return (BO.Volunteer?)GetValue(CurrentVolunteerProperty); }
+            set { SetValue(CurrentVolunteerProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentVolunteerProperty =
+            DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(VolunteerWindow), new PropertyMetadata(null));
+        
+        public string ButtonText
+        {
+            get => (string)GetValue(ButtonTextProperty);
+            set => SetValue(ButtonTextProperty, value);
+        }
+
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register("ButtonText", typeof(string), typeof(VolunteerWindow));
 
     }
 }
