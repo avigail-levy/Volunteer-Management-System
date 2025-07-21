e8c115ddb74ee27906a3d26d37c5a85a416037ea
avigail-levy <avigail6282@gmail.com>
2025-07-21 03:28:15 +0300

    Extras

--- DalList/DalList .cs
+++ DalList/DalList .cs
@@ -1,25 +1,30 @@
 ﻿namespace Dal;
 using DalApi;
+
 sealed internal class DalList : IDal
 {
     private DalList() { }
-    public static IDal Instance { get; } = new DalList();
-    public IVolunteer Volunteer { get; }= new VolunteerImplementation();
+
+    public static IDal Instance => Nested.instance;
+
+    private static class Nested
+    {
+        internal static readonly IDal instance = new DalList();
+    }
+
+    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
     public ICall Call { get; } = new CallImplementation();
-
     public IAssignment Assignment { get; } = new AssignmentImplementation();
-
     public IConfig Config { get; } = new ConfigImplementation();
+
     /// <summary>
-    ///reset all entities
+    /// reset all entities
     /// </summary>
     public void ResetDB()
     {
         Volunteer.DeleteAll();
         Call.DeleteAll();
-        Assignment.DeleteAll();	  
+        Assignment.DeleteAll();
         Config.Reset();
     }
-
 }
-
--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,23 +1,27 @@
 ﻿using DalApi;
-using System.Diagnostics;
 namespace Dal;
+
 sealed internal class DalXml : IDal
 {
     private DalXml() { }
-    public static IDal Instance { get; } = new DalXml();
+
+    public static IDal Instance => Nested.instance;
+
+    private static class Nested
+    {
+        internal static readonly IDal instance = new DalXml();
+    }
+
     public IVolunteer Volunteer { get; } = new VolunteerImplementation();
-
     public ICall Call { get; } = new CallImplementation();
-
     public IAssignment Assignment { get; } = new AssignmentImplementation();
-
     public IConfig Config { get; } = new ConfigImplementation();
 
     public void ResetDB()
     {
         Volunteer.DeleteAll();
         Assignment.DeleteAll();
-        Call.DeleteAll(); 
+        Call.DeleteAll();
         Config.Reset();
     }
 }
